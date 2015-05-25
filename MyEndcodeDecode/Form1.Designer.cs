1a83a75caccd8726c6c351aa019fc918d599aec7
PhucVV <phuc.se.uit@gmail.com>
2015-05-25 09:20:11 +0700

    moi

---
+++ MyEndcodeDecode/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace MyEndcodeDecode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB_in = new System.Windows.Forms.RichTextBox();
+            this.TB_out = new System.Windows.Forms.RichTextBox();
+            this.BT_endcode = new System.Windows.Forms.Button();
+            this.BT_decode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TB_in
+            // 
+            this.TB_in.Location = new System.Drawing.Point(2, 3);
+            this.TB_in.Name = "TB_in";
+            this.TB_in.Size = new System.Drawing.Size(659, 96);
+            this.TB_in.TabIndex = 0;
+            this.TB_in.Text = "";
+            // 
+            // TB_out
+            // 
+            this.TB_out.Location = new System.Drawing.Point(2, 242);
+            this.TB_out.Name = "TB_out";
+            this.TB_out.Size = new System.Drawing.Size(659, 96);
+            this.TB_out.TabIndex = 1;
+            this.TB_out.Text = "";
+            // 
+            // BT_endcode
+            // 
+            this.BT_endcode.Location = new System.Drawing.Point(51, 142);
+            this.BT_endcode.Name = "BT_endcode";
+            this.BT_endcode.Size = new System.Drawing.Size(71, 43);
+            this.BT_endcode.TabIndex = 2;
+            this.BT_endcode.Text = "EndCode";
+            this.BT_endcode.UseVisualStyleBackColor = true;
+            this.BT_endcode.Click += new System.EventHandler(this.BT_endcode_Click);
+            // 
+            // BT_decode
+            // 
+            this.BT_decode.Location = new System.Drawing.Point(166, 142);
+            this.BT_decode.Name = "BT_decode";
+            this.BT_decode.Size = new System.Drawing.Size(71, 43);
+            this.BT_decode.TabIndex = 3;
+            this.BT_decode.Text = "Decode";
+            this.BT_decode.UseVisualStyleBackColor = true;
+            this.BT_decode.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BT_decode_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 358);
+            this.Controls.Add(this.BT_decode);
+            this.Controls.Add(this.BT_endcode);
+            this.Controls.Add(this.TB_out);
+            this.Controls.Add(this.TB_in);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TB_in;
+        private System.Windows.Forms.RichTextBox TB_out;
+        private System.Windows.Forms.Button BT_endcode;
+        private System.Windows.Forms.Button BT_decode;
+    }
+}
+
---
+++ MyEndcodeDecode/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyEndcodeDecode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BT_decode_MouseClick(object sender, MouseEventArgs e)
+        {
+            TB_out.Text =decode(TB_in.Text);
+        }
+
+        private void BT_endcode_Click(object sender, EventArgs e)
+        {
+            TB_out.Text = endcode(TB_in.Text);
+        }
+
+        private string RandomString(int size)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var stringChars = new char[size];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new String(stringChars);
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Base64Decode(string base64EncodedData)
+        {
+            int mod4 = base64EncodedData.Length % 4;
+            if (mod4 > 0)
+            {
+                base64EncodedData += new string('=', 4 - mod4);
+            }
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        private string endcode(string _s)
+        {
+            string s = _s;
+            s = Base64Encode(s);
+            s=s.Replace("=", "");
+            s += RandomString(2);
+            s = Base64Encode(s);
+            s = s.Replace("=", "");
+            s += RandomString(3);
+            s = Base64Encode(s);
+            s = s.Replace("=", "");
+            s += RandomString(9);
+            return s;
+        }
+
+        private string decode(string _s)
+        {
+            string s = _s;
+            s = s.Substring(0, s.Length - 9);
+            s = Base64Decode(s);
+            s = s.Substring(0, s.Length - 3);
+            s = Base64Decode(s);
+            s = s.Substring(0, s.Length - 2);
+            s = Base64Decode(s);
+            return s;
+        }
+    }
+}
